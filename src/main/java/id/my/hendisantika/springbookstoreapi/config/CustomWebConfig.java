0c8d68bd3b2dce665900fdf4942bca30e6483b99
hendisantika <hendisantika@yahoo.co.id>
2024-05-11 07:51:22 +0700

    Add addInterceptors in CustomWebConfig🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbookstoreapi/config/CustomWebConfig.java
+++ src/main/java/id/my/hendisantika/springbookstoreapi/config/CustomWebConfig.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.data.web.SortHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
 import java.util.List;
 
@@ -41,4 +42,10 @@ public class CustomWebConfig {
 
         argumentResolvers.add(pageResolver);
     }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor);
+    }
+
 }
