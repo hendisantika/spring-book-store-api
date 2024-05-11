cb6f070da1453d92efe5ddf68e5202ad88d3bd05
hendisantika <hendisantika@yahoo.co.id>
2024-05-11 07:49:25 +0700

    Add handleException in GlobalExceptionHandler🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbookstoreapi/common/GlobalExceptionHandler.java
+++ src/main/java/id/my/hendisantika/springbookstoreapi/common/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package id.my.hendisantika.springbookstoreapi.common;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,5 +17,13 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
  */
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity handleException(Exception e) {
 
+        APIResponse apiResponse = new APIResponse();
+        apiResponse.setError("Oops..Something went wrong!");
+        apiResponse.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(apiResponse);
+    }
 }
