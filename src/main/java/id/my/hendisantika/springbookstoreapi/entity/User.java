837822db539bf972f0dce87a41679e20d7304004
hendisantika <hendisantika@yahoo.co.id>
2024-05-11 07:46:03 +0700

    Add User Entity🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbookstoreapi/entity/User.java
@@ -0,0 +1,47 @@
+package id.my.hendisantika.springbookstoreapi.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+import org.joda.time.DateTime;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-book-store-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/11/24
+ * Time: 07:45
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String gender;
+    private String emailId;
+    private String phoneNumber;
+    private String userType = Constant.USER_TYPE.NORMAL;
+    private String password;
+    private Boolean isActive = true;
+    private Integer loginCount = 0;
+    private String ssoType;
+    private DateTime loginAt;
+    @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime")
+    private DateTime createdAt;
+    @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime")
+    private DateTime updatedAt;
+
+
+}
