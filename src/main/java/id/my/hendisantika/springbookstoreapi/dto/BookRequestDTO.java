fd3d2624ab185bfb59616fd77d60efd70666709f
hendisantika <hendisantika@yahoo.co.id>
2024-05-11 12:37:00 +0700

    Add BookRequestDTO🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbookstoreapi/dto/BookRequestDTO.java
@@ -0,0 +1,30 @@
+package id.my.hendisantika.springbookstoreapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-book-store-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/11/24
+ * Time: 12:36
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookRequestDTO {
+    private Long id;
+    private String name;
+    private String desc;
+    private Integer yearOfPublication;
+    private String bookType;
+
+    private List<BookEditionDTO> editions;
+}
