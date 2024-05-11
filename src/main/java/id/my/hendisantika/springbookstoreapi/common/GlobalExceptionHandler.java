11a11e661182c5e865b6745b713ef82aa13e4b01
hendisantika <hendisantika@yahoo.co.id>
2024-05-11 07:49:15 +0700

    Add GlobalExceptionHandler🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbookstoreapi/common/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.springbookstoreapi.common;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-book-store-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/11/24
+ * Time: 07:49
+ * To change this template use File | Settings | File Templates.
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+}
