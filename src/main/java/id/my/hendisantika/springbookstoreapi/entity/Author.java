4d2876618abd6934ebbcc0ba264540bc6c8c5d45
hendisantika <hendisantika@yahoo.co.id>
2024-05-11 07:43:21 +0700

    Add Author Entity🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbookstoreapi/entity/Author.java
@@ -0,0 +1,35 @@
+package id.my.hendisantika.springbookstoreapi.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.joda.time.DateTime;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-book-store-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/11/24
+ * Time: 07:41
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String gender;
+
+    private DateTime createdAt;
+    private DateTime updatedAt;
+}
