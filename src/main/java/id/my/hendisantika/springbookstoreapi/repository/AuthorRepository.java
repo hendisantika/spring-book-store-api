95d224a6bf98e69e568108f8b36f56903228b687
hendisantika <hendisantika@yahoo.co.id>
2024-05-11 12:43:27 +0700

    Add AuthorRepository🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbookstoreapi/repository/AuthorRepository.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.springbookstoreapi.repository;
+
+import id.my.hendisantika.springbookstoreapi.entity.Author;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-book-store-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/11/24
+ * Time: 12:43
+ * To change this template use File | Settings | File Templates.
+ */
+@Repository
+public interface AuthorRepository extends PagingAndSortingRepository<Author, Long> {
+
+    Page<Author> findAllByOrderByIdDesc(Pageable pageable);
+}
