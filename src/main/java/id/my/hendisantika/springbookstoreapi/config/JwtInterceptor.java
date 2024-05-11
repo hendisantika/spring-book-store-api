7670151c5ab7ca87a2c9be999be279281b5c90e1
hendisantika <hendisantika@yahoo.co.id>
2024-05-11 07:52:30 +0700

    Add preHandle in JwtInterceptor🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbookstoreapi/config/JwtInterceptor.java
+++ src/main/java/id/my/hendisantika/springbookstoreapi/config/JwtInterceptor.java
@@ -1,5 +1,8 @@
 package id.my.hendisantika.springbookstoreapi.config;
 
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -23,5 +26,23 @@ public class JwtInterceptor extends HandlerInterceptorAdapter {
 
     public JwtInterceptor(RequestMeta requestMeta) {
         this.requestMeta = requestMeta;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String auth = request.getHeader("authorization");
+
+        if (!(request.getRequestURI().contains("login") || request.getRequestURI().contains("signup"))) {
+            Claims claims = jwtUtils.verify(auth);
+
+            requestMeta.setUserName(claims.get("name").toString());
+            requestMeta.setUserId(Long.valueOf(claims.getIssuer()));
+            requestMeta.setUserType(claims.get("type").toString());
+            requestMeta.setEmailId(claims.get("emailId").toString());
+
+        }
+
+        return super.preHandle(request, response, handler);
     }
 }
