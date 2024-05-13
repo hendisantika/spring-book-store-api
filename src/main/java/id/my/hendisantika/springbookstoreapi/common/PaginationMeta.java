171a07e3f9a6f22caaf1b9170821405585f50f0d
hendisantika <hendisantika@yahoo.co.id>
2024-05-13 09:42:53 +0700

    Add createPagination in PaginationMeta 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbookstoreapi/common/PaginationMeta.java
+++ src/main/java/id/my/hendisantika/springbookstoreapi/common/PaginationMeta.java
@@ -3,6 +3,7 @@ package id.my.hendisantika.springbookstoreapi.common;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +25,17 @@ public class PaginationMeta {
     private Integer pageNumber;
     private Boolean isLast;
     private Boolean isFirst;
+
+    public static <T> PaginationMeta createPagination(Page<T> page) {
+        PaginationMeta paginationMeta = new PaginationMeta();
+
+        paginationMeta.setIsFirst(page.isFirst());
+        paginationMeta.setIsLast(page.isLast());
+        paginationMeta.setPageNumber(page.getNumber() + 1);
+        paginationMeta.setPageSize(page.getSize());
+        paginationMeta.setTotalCount(page.getTotalElements());
+        paginationMeta.setTotalPage(page.getTotalPages());
+
+        return paginationMeta;
+    }
 }
